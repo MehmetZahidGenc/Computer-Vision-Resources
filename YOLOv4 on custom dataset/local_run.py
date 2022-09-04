1c510173d2e589efafa3b3ead30decafd849dba9
Mehmet Zahid Genç <71969819+MehmetZahidGenc@users.noreply.github.com>
2022-09-04 13:54:42 +0300

    Add files via upload

---
+++ YOLOv4 on custom dataset/local_run.py
@@ -0,0 +1,77 @@
+# to run on local machine we need 3 files;
+    # obj.names
+    # .cfg file (your custom cfg file)
+    # .weights file (your custom weights)
+
+
+import cv2 as cv
+import numpy as np
+
+cap = cv.VideoCapture(0)
+whT = 416
+confThreshold = 0.5
+nmsThreshold = 0.2
+
+# Load model
+
+classesFile = 'obj.names'
+classNames = []
+
+with open(classesFile, 'rt') as f:
+    classNames = f.read().rstrip('n').split('n')
+
+
+## Model files
+modelConfiguration = 'yolov4-custom.cfg'
+modelWeights = 'yolov4-custom_last.weights'
+
+net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+
+
+def findObjects(outputs, img):
+    hT, wT, cT = img.shape
+    bbox = []
+    classIds = []
+    confs = []
+    for output in outputs:
+        for det in output:
+            scores = det[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > confThreshold:
+                w, h = int(det[2] * wT), int(det[3] * hT)
+                x, y = int((det[0] * wT)-w / 2), int((det[1] * hT)-h / 2)
+                bbox.append([x, y, w, h])
+                classIds.append(classId)
+                confs.append(float(confidence))
+
+    indices = cv.dnn.NMSBoxes(bbox, confs, confThreshold, nmsThreshold)
+
+    for i in indices:
+        i = i[0]
+        box = bbox[i]
+        x, y, w, h = box[0], box[1], box[2], box[3]
+
+        cv.rectangle(img, (x, y), (x+w, y+h), (255, 0, 255), 2)
+        cv.putText(img, f'{classNames[classIds[i]].upper()} {int(confs[i] * 100)}%',
+                   (x, y-10), cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
+
+
+while True:
+    success, img = cap.read()
+    blob = cv.dnn.blobFromImage(img, 1 / 255, (whT, whT), [0, 0, 0], 1, crop=False)
+    net.setInput(blob)
+    layersNames = net.getLayerNames()
+    outputNames = [(layersNames[i[0]-1]) for i in net.getUnconnectedOutLayers()]
+    outputs = net.forward(outputNames)
+    findObjects(outputs, img)
+
+    cv.imshow('img', img)
+
+    if cv.waitKey(16) == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
