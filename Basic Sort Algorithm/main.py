86969257bbc894fb9a0a24a1430e0147792ba51c
Mehmet Zahid Genç <71969819+MehmetZahidGenc@users.noreply.github.com>
2023-02-01 16:20:53 +0300

    Update main.py

--- Basic Sort Algorithm/main.py
+++ Basic Sort Algorithm/main.py
@@ -3,12 +3,6 @@ import numpy as np
 from ZahidTrack import ZahidSort
 
 sorting_algorithm = ZahidSort()
-liste = ['img.png']
 
-for img_names in liste:
-    img = cv2.imread(img_names)
-    detect_ids = sorting_algorithm.runner([[60, 35, 335, 140]], main_frame=img) # bbox: [[x1,y1,w1,h1], [x2,y2,w2,h2]]
-    enhanced_image = sorting_algorithm.enhance_visuality(bbox_list=[[60, 35, 335, 140]], detected_ids=detect_ids, main_frame=img, rect=True)
-    cv2.imshow('enhanced_image', enhanced_image)
-    cv2.waitKey(0)
-    print(f"Detected ids: {detect_ids}")
+detect_ids = sorting_algorithm.runner(bbox_list, main_frame=img) # bbox_list: [[x1,y1,w1,h1], [x2,y2,w2,h2]]
+enhanced_image = sorting_algorithm.enhance_visuality(bbox_list, detected_ids=detect_ids, main_frame=img, rect=True)
