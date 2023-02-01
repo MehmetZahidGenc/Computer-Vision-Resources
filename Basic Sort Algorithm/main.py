5a25ce84b0e3b59b2e89b7f229d8b0bd1dd89aeb
Mehmet Zahid Genç <71969819+MehmetZahidGenc@users.noreply.github.com>
2023-02-01 16:24:47 +0300

    Delete Basic Sort Algorithm directory

--- Basic Sort Algorithm/main.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-from ZahidTrack import ZahidSort
-
-sorting_algorithm = ZahidSort()
-
-detect_ids = sorting_algorithm.runner(bbox_list, main_frame=img) # bbox_list: [[x1,y1,w1,h1], [x2,y2,w2,h2]]
-enhanced_image = sorting_algorithm.enhance_visuality(bbox_list, detected_ids=detect_ids, main_frame=img, rect=True)
